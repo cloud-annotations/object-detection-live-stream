9d22a8b0c07ac08ac8356738033c409d6b2de7c3
Nick <bourdakos1@gmail.com>
2019-10-08 11:44:14 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
